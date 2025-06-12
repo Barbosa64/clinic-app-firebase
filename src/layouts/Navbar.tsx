9b586570a6eb4bbf727cb28714d340159e86b6ee
Barbosa64 <rui.barbosa64@gmail.com>
2025-06-12 18:52:32 +0100

    navbar perfil removed

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -86,13 +86,6 @@ const Navbar = () => {
 							<MenuItems className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden'>
 								<MenuItem>
 									{({ active }) => (
-										<Link to='/perfil' className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-											Perfil
-										</Link>
-									)}
-								</MenuItem>
-								<MenuItem>
-									{({ active }) => (
 										<Link to='/configuracoes' className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
 											Configurações
 										</Link>
